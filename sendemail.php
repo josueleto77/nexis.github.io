42ddc0541c476c2e54ecdabfdff18c7961f843bb
josueleto77 <94468870+josueleto77@users.noreply.github.com>
2021-12-19 20:06:06 -0500

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -5,17 +5,17 @@
 		'message'=>'Thank you for contacting us. We will contact you as early as possible.'
 	);
 	
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $phone = $_POST['phone']; 
-    $company = @trim(stripslashes($_POST['company'])); 
+    $name = @trim(stripslashes($_POST['your name'])); 
+    $email = @trim(stripslashes($_POST['your email'])); 
+    $phone = $_POST['your phone']; 
+    $company = @trim(stripslashes($_POST[''])); 
     $subject = @trim(stripslashes($_POST['subject'])); 
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = 'no_reply@nxispwr.com';
     $email_to = 'jgalindo@nxispwr.com';//replace with your email
 
-    $body = 'The following person: [ ' . $name . ' ] sent you a message via the www.nxispwr.com website.' . "\n\n" . 'The following is their contact information:' . "\n" . 'Email:     ' . $email . "\n" . 'Telephone: ' . $phone . "\n" . 'Company:   ' . $company . "\n" . 'Subject:   ' . $subject . "\n\n" . 'Message:   <<< ' . $message . " >>>\n\n" . 'DO NOT REPLY TO THIS EMAIL.';
+    $body = 'The following person: [ ' . $name . ' ] sent you a message via the nexispower.com website.' . "\n\n" . 'The following is their contact information:' . "\n" . 'Email:     ' . $email . "\n" . 'Telephone: ' . $phone . "\n" . 'Company:   ' . $company . "\n" . 'Subject:   ' . $subject . "\n\n" . 'Message:   <<< ' . $message . " >>>\n\n" . 'DO NOT REPLY TO THIS EMAIL.';
 
     $success = @mail($email_to, 'e-mail sent from nexispower.com', $body, 'From: <'.$email_from.'>');
 	
